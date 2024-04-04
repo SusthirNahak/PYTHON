74c7ed88bc46b1af835814d0c84479ef048592b1
susthir <susthir19march@gmail.com>
2024-04-04 22:25:46 +0530

    python data types

---
+++ Datatype & Variables/one.py
@@ -0,0 +1,12 @@
+print("Hello World!!!","kanha")
+#variable- is a memory location ,where we store data in a programing language
+name="kanha"
+age=26
+sal=45000
+print(age,sal,name)
+print("my name is ",name ,"my salary is",sal)
+"""
+----------------
+this is the multiline comments
+----------------
+"""
---
+++ Datatype & Variables/two.py
@@ -0,0 +1,12 @@
+name="Amrut"
+print(type(name))
+
+amount=45.90
+print(type(amount))
+
+#DataTypes
+#1.Integer
+#2.Boolean
+#3.Float
+#4.String
+#5.None

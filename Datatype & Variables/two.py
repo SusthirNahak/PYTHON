16efad05b3eef8ea160df53ad732a9cc8a40384b
susthir <susthir19march@gmail.com>
2024-04-23 01:37:33 +0530

    update

--- Datatype & Variables/two.py
+++ Datatype & Variables/two.py
@@ -14,3 +14,39 @@ def num(n):
     print(n)
 
 num("hyyy")    
+l1="Rahul Gandhi".split()
+print(type(l1))
+print(l1)
+
+h1=[10,20,30]
+h2=h1.copy()
+
+print(type(h2))
+
+enames=['Rahul','Sonia','Priyanka',"Modi"]
+for ename in enames:
+    print(ename)
+
+i=0
+while i<=len(enames)-1:
+    print(enames[i])
+    i=i+1
+
+usernames=['Rahuli','Sonia','Priyanka',"Modi"]
+usernames.append('amitsaha')
+print(usernames)
+
+
+
+polnames=['Rahulw', 'Sonia', 'Priyankaw', 'Modiw', 'Amithw']
+#         0        1         2          3       4
+# insert "Rajiv Gandhi" at index 1 - using index method
+#what is list index method. insert elemnt at specified index value
+
+polnames.insert(2,"rajivw")
+print(polnames)
+
+r1=[10,60,30]
+r2=[52,78,74]
+r3=r1.extend(r2)
+print(r3)
---
+++ listobj/list.py
@@ -0,0 +1,9 @@
+enames=['Rahul','Sonia','Priyanka','Modi','Amith','KCR']
+# index   0        1           2       3    4       5
+#what is slice operator
+
+#new_enames=enames[2:5]
+new_enames=enames[3:6]
+
+print(enames)
+print(new_enames)
